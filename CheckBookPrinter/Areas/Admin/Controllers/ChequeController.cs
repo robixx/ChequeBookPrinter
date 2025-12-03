3611a83e510b0fb5671a0c1e019ac5f2117cc692
Shoriful Islam Robi <robi@itcbd.com>
2025-12-03 16:49:16 +0600

    chequebookprinter preoject

---
+++ CheckBookPrinter/Areas/Admin/Controllers/ChequeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheckBookPrinter.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChequeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CheckBookPrinter/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheckBookPrinter.Areas.Dashboard.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
